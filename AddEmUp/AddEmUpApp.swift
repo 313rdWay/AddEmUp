ef2507e89e4e8545059d172f80bcf712e86b29f8
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-08-12 19:27:47 -0400

    Initial Commit

---
+++ AddEmUp/AddEmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddEmUpApp.swift
+//  AddEmUp
+//
+//  Created by Davaughn Williams on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AddEmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AddEmUpTests/AddEmUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  AddEmUpTests.swift
+//  AddEmUpTests
+//
+//  Created by Davaughn Williams on 8/12/25.
+//
+
+import Testing
+@testable import AddEmUp
+
+struct AddEmUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
