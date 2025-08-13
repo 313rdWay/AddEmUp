e7701b01d274e4e45b6b8b4e8debc1246070f7ff
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-08-13 18:47:25 -0400

    code clean up

--- AddEmUp/Views/CalculatorView.swift
+++ AddEmUp/Views/CalculatorView.swift
@@ -16,25 +16,11 @@ struct CalculatorView: View {
             
             Spacer()
             
-            ZStack(alignment: .center) {
-                Text(vm.displayText)
-                    .font(.system(size: 75))
-                    .fontWeight(.bold)
-                    .foregroundStyle(.black)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    .padding()
-                
-                RoundedRectangle(cornerRadius: 25)
-                    .stroke(style: StrokeStyle(lineWidth: 10))
-                    
-            }
-            .frame(height: 200)
-            .padding(.horizontal)
-            
+            displayView
             
             Spacer()
             
-            KeyboardView()
+            keyboardView
             
             Spacer()
         }
@@ -44,4 +30,38 @@ struct CalculatorView: View {
 #Preview {
     CalculatorView()
         .environmentObject(CalculatorViewModel())
+}
+
+extension CalculatorView {
+    private var displayView: some View {
+        ZStack(alignment: .center) {
+            Text(vm.displayText)
+                .font(.system(size: 75))
+                .fontWeight(.bold)
+                .foregroundStyle(.black)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding()
+            
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(style: StrokeStyle(lineWidth: 10))
+                
+        }
+        .frame(height: 200)
+        .padding(.horizontal)
+    }
+    
+    private var keyboardView: some View {
+        VStack(spacing: -25) {
+            ForEach(vm.buttonLayout, id: \.self) { row in
+                HStack(spacing: -25) {
+                    ForEach(row, id: \.self) { item in
+                        ButtonView(label: item,
+                                   backgroundColor: self.vm.colorForButton(item),
+                                   action: {vm.handleButtonPress(item)
+                        })
+                    }
+                }
+            }
+        }
+    }
 }
